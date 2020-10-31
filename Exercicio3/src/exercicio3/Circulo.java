50657f624dc44d2f6155bca16dd0b715b8dc49a8
Marcelo Pedroni da Silva <marcelopedronidasilva@gmail.com>
2020-10-31 11:56:19 -0200

    Exercício 3, criando uma classe Java com métodos para calcular área e perímetro de formas geométricas, com retornos 0 para métodos double (getArea() e getPerimetro()) e null para o método getNome() do tipo String. Criando classes java com as formas Circulo, Triangulo, Quadrado e Retangulo com seus atributos mínimos necessários que herdam os métodos e atributos de Forma.

---
+++ Exercicio3/src/exercicio3/Circulo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio3;
+
+/**
+ *
+ * @author LocalHost
+ */
+public class Circulo extends Forma {
+    private double raio;
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+    
+}
---
+++ Exercicio3/src/exercicio3/Quadrado.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio3;
+
+/**
+ *
+ * @author LocalHost
+ */
+public class Quadrado extends Forma{
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+    
+}
